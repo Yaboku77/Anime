c97760b659486e69d80f6e1445f85a26d923540f
Yaboku77 <90348922+Yaboku77@users.noreply.github.com>
2023-10-03 22:52:50 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 let buttons = [];
 
-    function getAnimeData(animeId) {
+    function getData(animeId) {
       let url = `https://api.jikan.moe/v4/anime/${animeId}`;
       return fetch(url)
         .then(response => {
@@ -12,69 +12,121 @@ let buttons = [];
         });
     }
 
+    function getCharacterData(animeId) {
+      let characterUrl = `https://api.jikan.moe/v4/anime/${animeId}/characters`;
+      return fetch(characterUrl)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Failed to fetch character data.');
+          }
+        });
+    }
+    
+    function getStaffData(animeId) {
+      let staffUrl = `https://api.jikan.moe/v4/anime/${animeId}/staff`;
+      return fetch(staffUrl)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Failed to fetch staff data.');
+          }
+        });
+    }
+
     function generateCode() {
-  const animeId = document.getElementById('animeId').value;
-  let selectedResolutions, selectedTypes; // Declare the variables here
+      const animeId = document.getElementById('animeId').value;
+      let selectedResolutions, selectedTypes; // Declare the variables here
 
-  getAnimeData(animeId)
-    .then(data => {
-      // Populate the selectedResolutions and selectedTypes variables
-      selectedResolutions = Array.from(document.getElementById('resolutionSelect').selectedOptions).map(option => option.value);
-      selectedTypes = Array.from(document.getElementById('typeSelect').selectedOptions).map(option => option.value);
-      
-      // Modify the score to be on a 0 to 100 scale
-      modifiedScore = data.data.score * 10;
+      getData(animeId)
+        .then(Data => {
+          // Populate the selectedResolutions and selectedTypes variables
+          selectedResolutions = Array.from(document.getElementById('resolutionSelect').selectedOptions).map(option => option.value);
+          selectedTypes = Array.from(document.getElementById('typeSelect').selectedOptions).map(option => option.value);
+          
+          // Modify the score to be on a 0 to 100 scale
+          const modifiedScore = Data.data.score * 10;
 
-      // Display data in generatedDataBox1
-      const generatedDataBox1 = document.getElementById('generatedDataBox1');
-      generatedDataBox1.textContent = data.data.title;
+          // Display data in generatedDataBox1 and generatedDataBox2
+          const generatedDataBox1 = document.getElementById('generatedDataBox1');
+          generatedDataBox1.textContent = Data.data.title;
 
-      // Display data in generatedDataBox2
-      const generatedDataBox2 = document.getElementById('generatedDataBox2');
-      generatedDataBox2.textContent = `${data.data.type}, ${data.data.status.replace(/Currently Airing/g, "Airing").replace(/Finished Airing/g, "Completed")}, ${data.data.genres.map(genre => genre.name).join(', ')}, ${data.data.themes.map(theme => theme.name).join(', ')}, ${data.data.demographics.map(demographic => demographic.name).join(', ')},${data.data.producers.map(producer => producer.name).join(', ')},${data.data.licensors.map(licensor => licensor.name).join(', ')},${data.data.studios.map(studio => studio.name).join(', ')},${selectedTypes.join(', ')},${selectedResolutions.join(', ')}`;
+          const generatedDataBox2 = document.getElementById('generatedDataBox2');
+          generatedDataBox2.textContent = `${Data.data.type}, ${Data.data.status.replace(/Currently Airing/g, "Airing").replace(/Finished Airing/g, "Completed")}, ${Data.data.genres.map(genre => genre.name).join(', ')}, ${Data.data.themes.map(theme => theme.name).join(', ')}, ${Data.data.demographics.map(demographic => demographic.name).join(', ')}, ${Data.data.producers.map(producer => producer.name).join(', ')}, ${Data.data.licensors.map(licensor => licensor.name).join(', ')}, ${Data.data.studios.map(studio => studio.name).join(', ')}, ${selectedTypes.join(', ')}, ${selectedResolutions.join(', ')}`;
 
-      // Generate the full HTML code and display it in the original generatedCodeBox
-      const code = generateHTMLCode(data, selectedResolutions, selectedTypes);
-      document.getElementById('generatedCode').textContent = code;
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
+          // Fetch character and staff data
+          return Promise.all([
+            getCharacterData(animeId),
+            getStaffData(animeId)
+          ])
+          .then(([characterData, staffData]) => {
+            // Generate the full HTML code and display it in the original generatedCodeBox
+            const code = generateHTMLCode(Data, characterData, staffData, selectedResolutions, selectedTypes, modifiedScore);
+            document.getElementById('generatedCode').textContent = code;
+          });
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
 
-
-
-    function generateHTMLCode(data, selectedResolutions, selectedTypes) {
+    function generateHTMLCode(Data, characterData, staffData, selectedResolutions, selectedTypes, modifiedScore) {
       let buttonsHTML = '';
       for (const button of buttons) {
         buttonsHTML += `<a class="shortc-button small blue" href="${button.url}">${button.title}</a>`;
       }
 
+      const japaneseVoiceActors = characterData.data
+  .filter(character => character.voice_actors.some(va => va.language === "Japanese"))
+  .slice(0, 4)
+  .map(character => {
+    const japaneseVAs = character.voice_actors.filter(va => va.language === "Japanese");
+    // Access only the first Japanese voice actor
+    const firstJapaneseVA = japaneseVAs[0];
+    return `
+      <strong><a href="${character.character.url}" target="_blank" rel="noopener nofollow">${character.character.name}</a></strong> – <a href="${firstJapaneseVA.person.url}" target="_blank" rel="noopener nofollow">${firstJapaneseVA.person.name}</a><br />
+    `;
+  })
+  .join('');
+  
+
+      const staff = staffData.data
+        .slice(0, 4)
+        .map(staffMember => {
+          const position = staffMember.positions[0];
+          return `
+            <strong><a href="${staffMember.person.url}" target="_blank" rel="noopener nofollow">${staffMember.person.name}</a></strong> – ${position}<br />
+          `;
+        })
+        .join('');
+
       const code = `
-        <div>
+   <div>
           <a><img border="0" data-original-height="450" data-original-width="800" height="360" src="" width="640" /></a>
         </div>
 
         <div class="review_wrap">
           <div class="review-box review-bottom review-percentage" id="review-box">
-            <h2 class="review-box-header the-global-title">${data.data.title}</h2>
+            <h2 class="review-box-header the-global-title">${Data.data.title}</h2>
             
             <div class="review-item">
         <span
-          ><h5>MyAnimeList - ${data.data.score}</h5>
-          <span style="width:${modifiedScore}" data-width="${modifiedScore}"></span
+          ><h5>MyAnimeList - ${Data.data.score}</h5>
+          <span style="width:${modifiedScore}%" data-width="${modifiedScore}"></span
         ></span>
             </div>
             
             <div class="review-summary">
               <div class="review-final-score">
-                <h3>${data.data.score}</h3>
+                <h3>${Data.data.score}</h3>
 
                 <h4>Average Rating</h4>
               </div>
 
               <div class="review-short-summary">
-                <p>${data.data.synopsis}</p>
+                <p>${Data.data.synopsis}</p>
               </div>
             </div>
           </div>
@@ -85,44 +137,66 @@ let buttons = [];
             Information <span class="bi bi-chevron-down"></span>
           </h3>
           <div class="toggle-content" style="text-align: left;">
-              <strong>English:</strong> ${data.data.title_english}<br />
-              <strong>Japanese:</strong> ${data.data.title_japanese}<br />
-              <strong>Synonyms:</strong> ${data.data.title_synonyms.join(', ')}<br />
-              <strong>Type:</strong> ${data.data.type}<br />
-              <strong>Episodes:</strong> ${data.data.episodes}<br />
-              <strong>Status:</strong> <a href="/search/label/${data.data.status.replace(/Currently Airing/g, "Airing").replace(/Finished Airing/g, "Completed")}">${data.data.status.replace(/Currently Airing/g, "Airing").replace(/Finished Airing/g, "Completed")}</a><br />
-              <strong>Aired:</strong> ${data.data.aired.string}<br />
-              <strong>Season:</strong> ${data.data.season}<br />
-              <strong>Broadcast:</strong> ${data.data.broadcast.string}<br />
-              <strong>Producers:</strong> ${data.data.producers.map(producer => `<a href="/search/label/${producer.name}">${producer.name}</a>`).join(', ')}<br />
-              <strong>Licensors:</strong> ${data.data.licensors.map(licensor => `<a href="/search/label/${licensor.name}">${licensor.name}</a>`).join(', ')}<br />
-              <strong>Studios:</strong> ${data.data.studios.map(studio => `<a href="/search/label/${studio.name}">${studio.name}</a>`).join(', ')}<br />
-              <strong>Source:</strong> ${data.data.source}<br />
-              <strong>Genres:</strong> ${data.data.genres.map(genre => `<a href="/search/label/${genre.name}">${genre.name}</a>`).join(', ')}<br />
-              <strong>Themes:</strong> ${data.data.themes.map(theme => `<a href="/search/label/${theme.name}">${theme.name}</a>`).join(', ')}<br />
-              <strong>Demographics:</strong> ${data.data.demographics.map(demographic => `<a href="/search/label/${demographic.name}">${demographic.name}</a>`).join(', ')}<br />
-              <strong>Duration:</strong> ${data.data.duration}<br />
-              <strong>Rating:</strong> ${data.data.rating}<br />
+              <strong>English:</strong> ${Data.data.title_english}<br />
+              <strong>Japanese:</strong> ${Data.data.title_japanese}<br />
+              <strong>Synonyms:</strong> ${Data.data.title_synonyms.join(', ')}<br />
+              <strong>Type:</strong> ${Data.data.type}<br />
+              <strong>Episodes:</strong> ${Data.data.episodes}<br />
+              <strong>Status:</strong> <a href="/search/label/${Data.data.status.replace(/Currently Airing/g, "Airing").replace(/Finished Airing/g, "Completed")}">${Data.data.status.replace(/Currently Airing/g, "Airing").replace(/Finished Airing/g, "Completed")}</a><br />
+              <strong>Aired:</strong> ${Data.data.aired.string}<br />
+              <strong>Season:</strong> ${Data.data.season}<br />
+              <strong>Broadcast:</strong> ${Data.data.broadcast.string}<br />
+              <strong>Producers:</strong> ${Data.data.producers.map(producer => `<a href="/search/label/${producer.name}">${producer.name}</a>`).join(', ')}<br />
+              <strong>Licensors:</strong> ${Data.data.licensors.map(licensor => `<a href="/search/label/${licensor.name}">${licensor.name}</a>`).join(', ')}<br />
+              <strong>Studios:</strong> ${Data.data.studios.map(studio => `<a href="/search/label/${studio.name}">${studio.name}</a>`).join(', ')}<br />
+              <strong>Source:</strong> ${Data.data.source}<br />
+              <strong>Genres:</strong> ${Data.data.genres.map(genre => `<a href="/search/label/${genre.name}">${genre.name}</a>`).join(', ')}<br />
+              <strong>Themes:</strong> ${Data.data.themes.map(theme => `<a href="/search/label/${theme.name}">${theme.name}</a>`).join(', ')}<br />
+              <strong>Demographics:</strong> ${Data.data.demographics.map(demographic => `<a href="/search/label/${demographic.name}">${demographic.name}</a>`).join(', ')}<br />
+              <strong>Duration:</strong> ${Data.data.duration}<br />
+              <strong>Rating:</strong> ${Data.data.rating}<br />
               <strong>Resolution:</strong> ${selectedResolutions.map(resolution => `<a href="/search/label/${resolution}">${resolution}</a>`).join(', ')}<br />
               <strong>Type:</strong> ${selectedTypes.map(Type => `<a href="/search/label/${Type}">${Type}</a>`).join(', ')}<br />
           </div>
         </div>
-
-        <div class="box download">
+        
+   <div class="toggle tie-sc-close">
+    <h3 class="toggle-head">
+      Characters &amp; Voice Actors
+      <span class="bi bi-chevron-down rotate" aria-hidden="true"></span>
+    </h3>
+    <div class="toggle-content"">
+      ${japaneseVoiceActors}
+      <a style="float: right;" href="${Data.data.url}/characters" target="_blank" rel="noopener nofollow">More &gt;&gt;</a><br>
+    </div>
+  </div>
+  <div class="toggle tie-sc-close">
+    <h3 class="toggle-head">
+      Staff <span class="bi bi-chevron-down rotate" aria-hidden="true"></span>
+    </h3>
+    <div class="toggle-content">
+      ${staff}
+      <a style="float: right;" href="${Data.data.url}/characters#staff" target="_blank" rel="noopener nofollow">More &gt;&gt;</a><br>
+    </div>
+  </div>
+  
+  <div class="box download">
           <div class="box-inner-block">
             <span class="fa tie-shortcode-boxicon"></span>To access the private drive just open Google Group and join the Google Group, ignore the rest.<br />
             After that, Enjoy&#8230; <a href="https://groups.google.com/g/bondgreninja" target="_blank" rel="noopener">Here</a>
           </div>
         </div>
-
-        <div class="toggle tie-sc-open">
+        
+  
+  <div class="toggle tie-sc-open">
           <h3 class="toggle-head">
             Google Drive <span aria-hidden="true" class="bi bi-chevron-down"></span>
           </h3>
           <div class="toggle-content" style="text-align: center;">
             ${buttonsHTML}
           </div>
-        </div>`;
+        </div>
+        `;
 
       return code;
     }
@@ -130,44 +204,23 @@ let buttons = [];
     function addButton() {
       const title = prompt('Enter button title:');
       const url = prompt('Enter button URL:');
-
-      buttons.push({ title, url });
-
-      const addedButtons = document.getElementById('addedButtons');
-      const buttonElement = document.createElement('button');
-      buttonElement.textContent = title;
-      buttonElement.onclick = () => {
-        editButton(title, url);
-      };
-      addedButtons.appendChild(buttonElement);
-
-      const codeBox = document.getElementById('generatedCodeBox');
-      const code = generateHTMLCode();
-      codeBox.textContent = code;
-    }
-
-    function editButton(title, url) {
-      const newTitle = prompt('Enter new button title:', title);
-      const newUrl = prompt('Enter new button URL:', url);
-
-      const button = buttons.find(btn => btn.title === title && btn.url === url);
-      if (button) {
-        button.title = newTitle;
-        button.url = newUrl;
+      if (title && url) {
+        buttons.push({ title, url });
+        const addedButtons = document.getElementById('addedButtons');
+        addedButtons.innerHTML = '';
+        for (const button of buttons) {
+          addedButtons.innerHTML += `<button>${button.title}</button>`;
+        }
       }
-
-      const codeBox = document.getElementById('generatedCodeBox');
-      const code = generateHTMLCode();
-      codeBox.textContent = code;
     }
 
     function copyGeneratedCode() {
-      const codeBox = document.getElementById('generatedCodeBox');
+      const generatedCode = document.getElementById('generatedCode');
       const range = document.createRange();
-      range.selectNode(codeBox);
+      range.selectNode(generatedCode);
       window.getSelection().removeAllRanges();
       window.getSelection().addRange(range);
       document.execCommand('copy');
       window.getSelection().removeAllRanges();
       alert('Code copied to clipboard!');
-          }
+                                                                                                                                                                                                                                                                                                                                                                                                                          }
